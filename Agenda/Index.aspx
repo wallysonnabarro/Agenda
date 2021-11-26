084059500e5b8c9d300329878eb786100f3ed6e2
Wallyson Alessandro <wallyson.a3@gmail.com>
2021-11-26 13:34:56 -0300

    First commit

---
+++ Agenda/Index.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Home.Master" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="Agenda.Index" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:Image ID="Image1" runat="server" ImageAlign="Middle" ImageUrl="~/Imagens/agenda2.ico" />
+&nbsp;
+</asp:Content>
---
+++ Agenda/Usuario.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Agenda
+{
+    public partial class Usuario : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SqlDataSourceUsuarios_Inserting(object sender, SqlDataSourceCommandEventArgs e)
+        {
+
+        }
+        protected void SqlDataSourceUsuarios_Inserted(object sender, SqlDataSourceStatusEventArgs e)
+        {
+            if (e.Exception != null)
+            {
+                e.ExceptionHandled = true;
+            }
+        }
+    }
+}
